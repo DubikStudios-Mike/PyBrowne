c2315c53caacfbf9657d657c8384cdcb80447909
DubikStudios-Mike <89711991+DubikStudios-Mike@users.noreply.github.com>
2021-09-24 19:58:21 -0230

    Create init.py

---
+++ PyBrowne/Users/Guest/Desktop/init.py
@@ -0,0 +1,24 @@
+'''from PyQt5 import QtWidgets as qtw
+
+class MainWindow(qtw.QWidget):
+
+    def __init__(self):
+
+        super().__init__()
+        self.setWindowTitle('PyQt5 - PyBrowne Desktop Environment INIT')
+
+        self.setLayout(qtw.QVBoxLayout())
+        # self.keypad()
+
+        self.show()
+    
+    def keypad(self):
+        pass
+
+pb = qtw.QApplication([])
+mw = MainWindow()
+'''
+
+print('WIP')
+
+# pb.exec_()
